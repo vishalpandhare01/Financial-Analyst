b41705f51a0581168d28f37bed393432d549d9a4
vishal pandhare <vishalp0@gmail.com>
2025-04-29 07:49:42 +0530

    user auth api updated

--- finance_analyst_api/accounts/views.py
+++ finance_analyst_api/accounts/views.py
@@ -3,23 +3,20 @@ from rest_framework import status, permissions, generics
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
-from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 
-logger = logging.getLogger(__name__)
-
-class RegisterView(APIView):
+# user register view
+class RegisterView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
     def post(self, request, *args, **kwargs):
-        logger.info("Received register request")
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            logger.error(f"Registration error: {serializer.errors}")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+# user login view
 class LoginView(generics.GenericAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = LoginSerializer
@@ -27,8 +24,8 @@ class LoginView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         email = request.data.get("email")
         password = request.data.get("password")
-        user = authenticate(email=email, password=password)
-
+        
+        user = LoginSerializer.CheckCredentials(email=email,password=password)
         if user:
             refresh = RefreshToken.for_user(user)
             return Response({
@@ -37,6 +34,7 @@ class LoginView(generics.GenericAPIView):
             })
         return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
 
+# user profile view
 class UserProfileView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
